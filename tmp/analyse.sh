bfe57aa929691c55375c24a752f4e758ab53fa7e
Francis Tyers <ftyers@prompsit.com>
2019-03-13 06:12:01 +0000

    add trigrams

--- tmp/analyse.sh
+++ tmp/analyse.sh
@@ -1,2 +1,3 @@
 for i in *.txt; do cat "${i}" | apertium -d ~/source/apertium//languages/apertium-kir/ kir-tagger > "${i}.tagged"; done
+for i in *.txt; do cat "${i}" | python3 ngrams.py 3 > "${i}.trigrams"; done
 
